0e124025163e5496f6082511250d503a9ac4e757
kudpig <shin.kudo.aa@gmail.com>
2020-08-26 06:01:02 +0900

    投稿CRUD機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,60 @@
 class PostsController < ApplicationController
+  before_action :require_login, only: %i[new create edit update destroy]
+  # require_loginメソッドにより、ログインしていないユーザは上記５つのアクションを実行できない
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, success: '投稿しました'
+    else
+      flash.now[:danger] = '投稿に失敗しました'
+      render :new
+    end
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+    # 他人が編集ページに遷移できなくするためにcurrent_userをつけている。
+    # 上記を設定しないと/posts/:id/editのid部分を変えたら入れてしまう。
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, success: '投稿を更新しました'
+    else
+      flash.now[:danger] = '投稿の更新に失敗しました'
+      render :edit
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    if @post.destroy
+      redirect_to posts_path, success: '投稿を削除しました'
+    else
+      # 万が一削除に失敗した場合はフラッシュを表示し、直前ページに戻る仕様にした。
+      # （削除場所がindexページとshowページの2パターンあるため）
+      flash.now[:danger] = '投稿の削除に失敗しました'
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body, images:[]).merge(user_id: current_user.id)
+    # images:[]とする事で、imagesを配列の形で扱うことができる
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'users#new'
+  root to: 'posts#index'
 
   resources :users, only: [:new, :create]
+  resources :posts
+
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
