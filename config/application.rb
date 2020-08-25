afcdc0b884cb6d6a8693c9cf2ab389854b87f1a3
kudpig <shin.kudo.aa@gmail.com>
2020-08-26 01:37:01 +0900

    rails-18nの導入・日本語設定

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module SelfStudySports
       g.test_framework false
       g.template_engine :slim
     end
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
