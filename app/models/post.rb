9623852d65fd7b6c1217c7370d683d0e028a6e28
kudpig <shin.kudo.aa@gmail.com>
2020-08-26 05:03:17 +0900

    modelアソシエーションの記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  # postは１つのuserを持つ、というアソシエーション
+  # この記載がないと、参照先(この場合はuser)テーブルにアクセスできない
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :posts
+  # userは複数のpostを持つというアソシエーション
+  # userとpost両方ともアソシエーションを記入する必要がある
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
