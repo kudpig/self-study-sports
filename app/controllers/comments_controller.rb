46cac9adad43e475780ca9565325d44d47aaeee8
kudpig <shin.kudo.aa@gmail.com>
2020-08-28 08:40:58 +0900

    コメントの機能(edit/update/destroy)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,31 @@ class CommentsController < ApplicationController
     # current_userを指定してコメントのcreateを実行する
   end
 
+  # editはshallowの対象
+  def edit
+    @comment = current_user.comments.find(params[:id])
+  end
+
+  # updateはshallowの対象
+  def update
+    @comment = current_user.comments.find(params[:id])
+    @comment.update(comment_update_params)
+  end
+
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy
+  end
 
   private
-    def comment_params
-      params.require(:comment).permit(:body).merge(user_id: current_user.id, post_id: params[:post_id])
-      # user_idとpost_idをcommentに紐づけて保存出来るようにする
-    end
+  
+  def comment_params
+    params.require(:comment).permit(:body).merge(user_id: current_user.id, post_id: params[:post_id])
+    # user_idとpost_idをcommentに紐づけて保存出来るようにする
+  end
 
+  def comment_update_params
+    params.require(:comment).permit(:body)
+    # updateするものはbodyのみ。それ以外の項目が変更されることは基本的にないため許可する必要もない
+  end
 end
