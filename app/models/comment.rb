5bcedd10da86105b5a5e8c747578b7e729f5090d
kudpig <shin.kudo.aa@gmail.com>
2020-08-27 20:50:53 +0900

    rails g controller comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :body, presence: true, length: { maximum: 255 }
-  # bodyはnullを許可していない。文字数にも制約をつけた。
+  # bodyは空入力を許可しない。文字数にも制約をつけた。
 end
