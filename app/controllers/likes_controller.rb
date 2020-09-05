368a51c4598b2f03a0c6f9e4883121007649cb88
kudpig <shin.kudo.aa@gmail.com>
2020-09-05 21:18:39 +0900

    コントローラーの記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    current_user.like(@post)
+    # userモデルで設定したlikeメソッド。(モデルにpost_idを渡すために1行目で@postを取得している)
+  end
+
+  def destroy
+    @post = Like.find(params[:id]).post
+    # 外そうとしている"いいね"のIDを取得し、そのlike_idに紐づくpostをを@postに格納
+    current_user.unlike(@post)
+    # userモデルで設定したunlikeメソッドを実行している。
+  end
 end
