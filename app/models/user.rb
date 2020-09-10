1644e9f6dc0aa09cf7a33ca65a24b9ffe9e0f01a
kudpig <shin.kudo.aa@gmail.com>
2020-09-10 22:24:47 +0900

    アソシエーションの記入(user.rb/relationship.rb)

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,27 @@ class User < ApplicationRecord
   # 「userがいいねしたpostを取得したい」ということであれば、"has_many :posts, through: :likes"でも良いように一見考えられるが、
   # userとpostの間には既にアソシエーションが組まれており、そこに新たな関係性を追加することはできないためlike_postsという項目を設定する必要がある。
 
+
+
+  # 【フォロー機能に関するアソシエーション】
+  # userとrelationshipについてアソシエーションを組む必要がある。簡潔に"has_many :relationships"としたい所だが、
+  # relationshipテーブルに２つあるカラムはどちらもuserテーブルに紐づく中間テーブルとなっており、どちらのカラムとのアソシエーションなのかをしっかり区別してあげる必要がある。
+  # よって、以下２パターンのアソシエーションを記述するということになる。
+  # ↓①
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  # "User"とRelationshipテーブルの「follower_id」とのアソシエーションを設定している。
+  # active_relationshipsとしているため、class_nameでテーブル名を明示。かつforeign_key設定により参照するカラムも指定。
+  has_many :following, through: :active_relationships, source: :followed
+  # "User"が「フォローをしている相手」を取得できるようにするための記述。
+  # 上記で設定した"active_relationships"を経由し、"followed_id"を持つuserを関連付ける。
+  # user.followingを実行する→→"active_relationships"を経由する(userは自身のidを元にRelationテーブル/follower_idを参照)→→sourceオプションにより、idが一致したレコードの"followed_id"データを取得できる
+  # ↓②
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+  # "User"が「フォローを受けている相手」を取得できるようにするための記述。
+  # 上記と考え方は同じ。違うのは、Userの"user_id"をRelationshipテーブルのどのカラムで探しているかということ。
+  # 今回は、user.followersを実行する→→"passive_relationships"を経由する(userは自身のidを元にRelationテーブル/followed_idを参照)→→sourceオプションにより、idが一致したレコードの"follower_id"データを取得できる
+
   def like(post)
     like_posts << post
     # current_userが持つ"like_posts"を配列形式とし、コントローラーより受け取った"postオブジェクト(いいねしようとしている投稿)"を
@@ -35,7 +56,7 @@ class User < ApplicationRecord
   def like?(post)
     like_posts.include?(post)
     # current_userが持つ"like_posts"にview(_like_area)より受け取った"postオブジェクト"が格納されているかどうかを確認する。
-    # 配列current_userが、postと等しい要素を持つ時にtrue、持たない時にfalseを返す。
+    # 配列like_postsが、postと等しい要素を持つ時にtrue、持たない時にfalseを返す。
   end
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
