6e9f4749857fe6f596e0a2d3c71bee01198c5832
kudpig <shin.kudo.aa@gmail.com>
2020-09-16 22:47:43 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index new create show]
   resources :posts do
+    collection do
+      get 'search'
+      # ネストすることでpostsコントローラーのsearchアクションを取得する。
+      # collectionによりposts全体を対象としているため、idを必要としないパスとなる。
+    end
     resources :comments, shallow: true
   end
   # shallowオプションについてはRailsガイド'ルーティング'の2.7.2を参考に記述
